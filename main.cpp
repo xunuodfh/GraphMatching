ccdf3d7990646eff715fa5e3eac7b4dd61b53842
三好先森 <42363640+xunuodfh@users.noreply.github.com>
2024-09-19 09:42:20 -0500

    Add files via upload
    getNeighbor返回地址版本

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ public:
     ~Timer() {
         auto end_time = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
-        std::cout << "Elapsed time: " << duration/1000/1000/60 << " minutes" << std::endl;
+        std::cout << "Elapsed time: " << (float)duration/1000/1000/60 << " minutes" << std::endl;
     }
 
 private:
@@ -43,6 +43,7 @@ void updateNodeCount(const string& filename){
             nodeCount = bigger;
         }
     }
+    nodeCount++;
 }
 // CSR数据结构
 struct CSR {
@@ -117,25 +118,43 @@ void printCSR(const CSR& csr) {
 }
 // 获取CSR中某个节点的邻居节点集合
 //这个要改成返回指针 uint32_t 首地址，和size
-unordered_set<int> getNeighbors(const CSR& csr, int vertex) {
-    unordered_set<int> neighbors;
+//unordered_set<int> getNeighbors(const CSR& csr, int vertex) {
+//    unordered_set<int> neighbors;
+//    int start = csr.rowPointers[vertex];
+//    int end = csr.rowPointers[vertex + 1];
+//    for (int i = start; i < end; ++i) {
+//        neighbors.insert(csr.colIndices[i]);
+//    }
+//    return neighbors;
+//}
+pair<const int*, int> getNeighbors(const CSR& csr, int vertex) {
+    // Find the start and end positions of the neighbors for the given vertex
     int start = csr.rowPointers[vertex];
     int end = csr.rowPointers[vertex + 1];
-    for (int i = start; i < end; ++i) {
-        neighbors.insert(csr.colIndices[i]);
-    }
-    return neighbors;
+
+    // Return the pointer to the first neighbor and the number of neighbors
+    return make_pair(&csr.colIndices[start], end - start);
 }
 
 // 计算两个集合的交集
 //改成stl库，指针版本
-unordered_set<int> intersect(const unordered_set<int>& set1, const unordered_set<int>& set2) {
-    unordered_set<int> result;
-    for (int elem : set1) {
-        if (set2.find(elem) != set2.end()) {
-            result.insert(elem);
-        }
-    }
+//unordered_set<int> intersect(const unordered_set<int>& set1, const unordered_set<int>& set2) {
+//    unordered_set<int> result;
+//    for (int elem : set1) {
+//        if (set2.find(elem) != set2.end()) {
+//            result.insert(elem);
+//        }
+//    }
+//    return result;
+//}
+vector<int> intersect(const int* set1_begin, const int* set1_end, const int* set2_begin, const int* set2_end) {
+    vector<int> result;
+    // 预分配结果向量的大小为两个集合大小中较小的那个，避免动态扩展
+//    result.reserve(min(set1_end - set1_begin, set2_end - set2_begin));
+
+    // 计算两个集合的交集，结果存储在 result 中
+    set_intersection(set1_begin, set1_end, set2_begin, set2_end, back_inserter(result));
+//    sort(result.begin(),result.end());
     return result;
 }
 
@@ -156,22 +175,27 @@ void output(int v0, int v1, int v2, int v3, set<tuple<int, int, int, int>>& uniq
 // 查找所有与查询图同构的子图
 void findIsomorphicSubgraphs(const CSR& csr) {
 //    int nodeCount = csr.rowPointers.size() - 1;  // 节点数
-    vector<unordered_set<int>> C(4); // 存储候选节点集
+//    vector<unordered_set<int>> C(4); // 存储候选节点集
     set<tuple<int, int, int, int>> uniqueSubgraphs;  // 存储已经输出的子图
 
     // C[0] = 所有节点
     for (int v0 = 0; v0 < nodeCount; ++v0) {
         // C[1] = N(v0)
-        C[1] = getNeighbors(csr, v0);
-        for (int v1 : C[1]) {
+//        C[1] = getNeighbors(csr, v0);
+        auto [neighbors_ptr_c0, size_c0] = getNeighbors(csr, v0);
+        for (int i = 0; i<size_c0; i++) {
             // C[2] = N(v0) ∩ N(v1)
-            C[2] = intersect(getNeighbors(csr, v0), getNeighbors(csr, v1));
-            for (int v2 : C[2]) {
+            auto [neighbors_ptr_c1, size_c1] = getNeighbors(csr, neighbors_ptr_c0[i]);
+            auto C2 = intersect(neighbors_ptr_c0,neighbors_ptr_c0+size_c0*sizeof(int),neighbors_ptr_c1,neighbors_ptr_c1+size_c1*sizeof(int));
+            for (int v2 : C2) {
                 // C[3] = N(v1) ∩ N(v2) − {v0}
-                C[3] = intersect(getNeighbors(csr, v1), getNeighbors(csr, v2));
-                C[3].erase(v0);
-                for (int v3 : C[3]) {
-                    output(v0, v1, v2, v3, uniqueSubgraphs);
+//                auto [neighbors_ptr_c2, size_c2] = getNeighbors(csr, neighbors_ptr_c1[i]);
+                auto [neighbors_ptr_c2, size_c2] = getNeighbors(csr, v2);
+                auto C3 = intersect(neighbors_ptr_c1,neighbors_ptr_c1+size_c1*sizeof(int), neighbors_ptr_c2,neighbors_ptr_c2+size_c2*sizeof(int));
+//                C3.erase(v0);
+                C3.erase(remove(C3.begin(), C3.end(), v0), C3.end());
+                for (int v3 : C3) {
+                    output(v0, neighbors_ptr_c0[i], v2, v3, uniqueSubgraphs);
                 }
             }
         }
